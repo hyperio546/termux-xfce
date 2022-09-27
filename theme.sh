b4557532424476da9d72db965f41bd5530e830d9
ButterTech <96462198+btrtech@users.noreply.github.com>
2022-09-27 20:34:39 +0530

    Update theme.sh

--- theme.sh
+++ theme.sh
@@ -3,14 +3,14 @@ cd
 mkdir themes-termux-xfce
 cd themes-termux-xfce
 echo "Installing themes!"
-apt install gtk2-engines-murrine -y
-git clone https://github.com/vinceliuice/Orchis-theme
+apt install gtk2-engines-murrine sassc -y
+git clone https://github.com/btrtech/Orchis-theme
 cd Orchis-theme
 ./install.sh
 git clone https://github.com/vinceliuice/Tela-circle-icon-theme
 cd Tela-circle-icon-theme
 ./install.sh
 apt install papirus-icon-theme
-git clone https://github.com/vinceliuice/Layan-gtk-theme
+git clone https://github.com/btrtech/Layan-gtk-theme
 cd Layan-gtk-theme
 ./install.sh
