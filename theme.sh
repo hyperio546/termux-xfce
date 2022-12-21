20552b4b73a0b9798c54c664b6ae7c1032d8544c
NeoTech <96462198+neoteche@users.noreply.github.com>
2022-12-21 14:35:33 +0530

    fixing file structure

--- theme.sh
+++ theme.sh
@@ -7,11 +7,14 @@ apt install gtk2-engines-murrine sassc -y
 git clone https://github.com/btrtech/Orchis-theme
 cd Orchis-theme
 ./install.sh
+cd ..
 git clone https://github.com/vinceliuice/Tela-circle-icon-theme
 cd Tela-circle-icon-theme
 ./install.sh
-apt install papirus-icon-theme
+cd ..
+apt install papirus-icon-theme -y
 git clone https://github.com/btrtech/Layan-gtk-theme
 cd Layan-gtk-theme
 ./install.sh
+cd
 echo "Themes are installed now!"
