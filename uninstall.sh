68a0cf413e0e00363a67f572e9cb1ca3369e9617
NeoTech <96462198+neoteche@users.noreply.github.com>
2022-12-27 22:33:30 +0530

    Yea boi

--- uninstall.sh
+++ uninstall.sh
@@ -1,1 +1,3 @@
-apt remove tigervnc xfce4 xfce4-terminal firefox && apt autoremove && apt clean && apt autoclean
+clear
+echo -e "\033[1mUndoing all changes...\033[0m"
+apt remove tigervnc xfce4 xfce4-terminal firefox && apt remove x11-repo && apt autoremove && apt clean && apt autoclean
