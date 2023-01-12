eee5fbd6ee6f166cb830d209f22d58e2b52c6f7b
Hyperio <96462198+hyperio546@users.noreply.github.com>
2023-01-12 11:34:22 +0530

    add pulseaudio in xstartup

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ pkg install xfce4 tigervnc -y
 clear
 echo -e "\033[1mLet's setup the VNC Server now. It will ask for a password. Put n and then press enter when asked for a read-only password\033[0m"
 vncserver && vncserver -kill :1 && echo "#!/data/data/com.termux/files/usr/bin/bash
-startxfce4 &" > /data/data/com.termux/files/home/.vnc/xstartup # sets up vncserver and writes xstartup
+startxfce4 & pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1" > /data/data/com.termux/files/home/.vnc/xstartup # sets up vncserver and writes xstartup
 chmod +x /data/data/com.termux/files/home/.vnc/xstartup # makes xstartup executable
 echo -e "\033[1mXFCE has been installed!\033[0m"
 clear
