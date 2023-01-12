4bbbc4cf76cfa179dccb1d2d63eaa3a3b34b4026
Hyperio <96462198+hyperio546@users.noreply.github.com>
2023-01-12 11:46:21 +0530

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,3 @@
 clear
 echo -e "\033[1mUndoing all changes...\033[0m"
-apt remove tigervnc xfce4 xfce4-terminal firefox pulseaudio -y && apt remove x11-repo -y && apt autoremove -y && apt clean && apt autoclean && cd && cd .config && rm -rf pulse xfce4 Thunar
+apt remove tigervnc xfce4 xfce4-terminal firefox pulseaudio -y && apt remove x11-repo -y && apt autoremove -y && apt clean && apt autoclean && cd && cd .config && rm -rf pulse/ xfce4/ Thunar/
