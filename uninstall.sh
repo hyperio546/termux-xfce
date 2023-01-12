560dfaea2cb0f3e6a04044c1828bb7da9fa613b1
Hyperio <96462198+hyperio546@users.noreply.github.com>
2023-01-12 22:35:01 +0530

    Improved and actually working uninstaller

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,26 @@
 clear
 echo -e "\033[1mUndoing all changes...\033[0m"
-apt remove tigervnc xfce4 xfce4-terminal firefox pulseaudio gtk2-engines-murrine -y && apt remove x11-repo -y && apt autoremove -y && apt clean && apt autoclean && cd && rm -rf themes-termux-xfce/ && rm -rf .vnc/ && cd .config && rm -rf pulse/ && rm -rf xfce4/ && rm -rf Thunar/ && rm -rf dconf/ && apt autoremove -y && rm -rf Downloads && rm -rf Documents && rm -rf Pictures && rm -rf Music && rm -rf Videos && rm -rf Desktop && rm -rf ~/.local/share/icons/
+echo -e "\033[1mRemoving packages...\033[0m"
+apt remove tigervnc xfce4 xfce4-terminal firefox pulseaudio gtk2-engines-murrine -y
+apt remove x11-repo -y
+apt autoremove -y
+apt clean
+apt autoclean
+echo -e "\033[1mRemoving directories\033[0m"
+cd
+rm -rf themes-termux-xfce/
+rm -rf .vnc/
+cd .config
+rm -rf pulse/
+rm -rf xfce4/
+rm -rf Thunar/
+rm -rf dconf/
+cd
+rm -rf Downloads/
+rm -rf Documents/
+rm -rf Pictures/
+rm -rf Music/
+rm -rf Videos/
+rm -rf Desktop/
+rm -rf ~/.local/share/icons/
+rm -rf .cache/
