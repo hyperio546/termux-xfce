42ea4809b54043279b769d793f9ceea29d5652ae
Hyperio <96462198+hyperio546@users.noreply.github.com>
2023-01-12 20:17:04 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
 clear
 echo -e "\033[1mInstalling packages please wait! Install time will depend on your device and network speed.\033[0m"
-# Update to get latest packages
-pkg update -y
+# upgrade for latest packages
+pkg upgrade -y
 pkg install x11-repo -y
-pkg install xfce4 tigervnc pulseaudio -y
+pkg install xfce4 tigervnc pulseaudio xfce4-goodies -y
 # Configuring it
 clear
 echo -e "\033[1mLet's setup the VNC Server now. It will ask for a password. Put n and then press enter when asked for a read-only password\033[0m"
