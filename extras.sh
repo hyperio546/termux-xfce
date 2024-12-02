aa530326a5c6366ec81942b01a392d642ac56cc5
Hyperio <96462198+hyperio546@users.noreply.github.com>
2024-12-02 19:40:32 +0530

    Desktop layout not ready yet.

--- extras.sh
+++ extras.sh
@@ -7,7 +7,7 @@ echo -e "\033[1mInstalling themes and icon packs...\033[0m"
 cd extras
 echo -e "\033[1mInstalling configs for XFCE and GTK\033[0m"
 sh ./theme.sh
-cp config/* ~/.config/ -r
+#cp config/* ~/.config/ -r
 cd
 echo -e "\033[1mDone.\033[0m"
 
